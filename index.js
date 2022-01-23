8aeae84a20d767b9af37261222ec8094868bbbad
_[N_F]_ <48897493+xtvlw@users.noreply.github.com>
2022-01-23 16:15:48 -0300

    features added
    all the features was added in the project, all the functions is working
    create character,
    update character,
    delete character

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 
 const url = new URL('http://0.0.0.0:3000/')
 
+let dataUser = {}
+
 let server = {
     method: "POST",
     headers: {
          "Content-Type": "application/json"
-    }
+    },
+    body: JSON.stringify({user_id: 'main'})
 }
+
 
 //create a json with caracters values, {name, age, breed, id}
 function create() {
@@ -27,70 +31,84 @@ function create() {
             caracter['breed'] = breed[arg].id
         }
     }
-    console.log(caracter.age)
     server['body'] = JSON.stringify(caracter)
     fetch(url+'create', server)
         .then(res => {
-            console.log(res)
+            alert(`caracter was created!`)
         })
 }
 
 //load the caracter to the select tag
-function onLoadUpdate () {
-    let select = document.querySelector('#caracters')
-
-    for (info in carcs) {
-        let op = document.createElement('option')
-        op.setAttribute('id', carcs[info].id)
-        op.setAttribute('class', 'op')
-        op.innerText = carcs[info].name
-        select.appendChild(op)
-    }
-
+function loadCaracters () {
+    let selectElem = document.querySelector('#caracters')
+    fetch(url+'getAll', server)
+        .then(server => server.json())
+        .then(caractersConfg => {
+            dataUser = caractersConfg
+            console.log(dataUser)
+            for (caracter in caractersConfg){
+                let optionElem = document.createElement('option')
+                optionElem.setAttribute('id', caractersConfg[caracter].id)
+                optionElem.innerText = caractersConfg[caracter].name
+                selectElem.appendChild(optionElem)
+        }
+    })
 }
 
 //change to the corrent selected caracter and update the display info
 function onUpdate () {
-    let index = document.getElementById('caracters').selectedIndex
-    let elem = document.getElementById('detail-info')
-    elem.innerHTML = ''
-    let infoCaracter = carcs[index]
+    let selectIndex = document.getElementById('caracters').selectedIndex
+    let listElem = document.getElementById('detail-info')
+    listElem.innerHTML = ''
+
+    let infoCaracter = dataUser[selectIndex]
     for (i in infoCaracter) {
         let inf = document.createElement('li')
         inf.innerText = `${i}: ${infoCaracter[i]}`
-        elem.appendChild(inf)
+        listElem.appendChild(inf)
     }
 }
 
 //generate a json with the values {name, age, breed, id}
 function update () {
-    let caracter = {}
+    let idElem = document.querySelector('#caracters')
+    let caracterId = idElem.options[idElem.selectedIndex].id
+    let newName = document.querySelector('#caracter-name').value
+    let newAge = parseInt(document.querySelector('#caracter-age').value)
+    let newBreed = document.getElementsByName('breed')
+    
+    let newCaracter = {}
 
-    let opSelect = document.getElementById('caracters').selectedIndex
-    let ops = document.getElementById('caracters').options
-    caracter['id'] = ops[opSelect].id
-
-    let name = document.querySelector('#caracter-name').value
-    let breed = document.getElementsByName('breed')
-    let age = parseInt(document.querySelector('#caracter-age').value)
-
-    if (name == '' || breed == '' || age == 0) {
-        alert('insert the caracter informations')
-    }
-
-    caracter['id'] = Math.random().toString()
-    caracter['name'] = name
-    caracter['age'] = age
-    for (arg in breed) {
-        if (breed[arg].checked) {
-            caracter['breed'] = breed[arg].id
+    for (breed in newBreed) {
+        if (newBreed[breed].checked){
+            newCaracter['breed'] = newBreed[breed].id //in the element id was declared as the breed
+            console.log(newCaracter)
         }
     }
+    newCaracter['age'] = newAge
+    newCaracter['name'] = newName
+    newCaracter['id'] = caracterId
+    newCaracter['user_id'] = dataUser.user_id
+
+    // sending to the server
+    server.body = JSON.stringify(newCaracter)
+    fetch(url+'update', server)
+        .then(console.log(`${newCaracter} was updated`))
+
+    location.reload()
 }
 
 //get the id to delete the caracter
 function DelCaracter () {
-    let opSelect = document.getElementById('caracters').selectedIndex
-    let ops = document.getElementById('caracters').options
-    let id = ops[opSelect].id
+    let idElem = document.querySelector('#caracters')
+    let id = {
+        id: idElem.options[idElem.selectedIndex].id,
+        user_id: dataUser[0].user_id
+    }
+    console.log(id)
+    server['body'] = JSON.stringify(id)
+    fetch(url+'delete', server)
+        .then(alert('DELETED'))
+
+    location.reload()
 }
